9aa42326971e73e479260c33e0674600623a1fd4
orlandev <onovas05@gmail.com>
2022-12-09 17:40:47 -0500

    PERFECT

--- app/src/main/java/com/orlandev/pokemonteam/MainActivity.kt
+++ app/src/main/java/com/orlandev/pokemonteam/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.orlandev.pokemonteam.ui.theme.PokemonTestTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
+import me.sargunvohra.lib.pokekotlin.model.NamedApiResource
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -24,7 +30,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val resultTest = remember {
+                        mutableStateOf<List<NamedApiResource>?>(null)
+                    }
+
+                    val scope = rememberCoroutineScope()
+
+                    LaunchedEffect(Unit) {
+                        scope.launch(Dispatchers.IO) {
+                            val a = PokeApiClient()
+                            val list = a.getRegionList(0, 100)
+                            resultTest.value = list.results
+                        }
+
+                    }
+
+                    resultTest.value?.let { result ->
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            items(result) {
+                                Text(text = it.name)
+                            }
+                        }
+                    }
                 }
             }
         }
